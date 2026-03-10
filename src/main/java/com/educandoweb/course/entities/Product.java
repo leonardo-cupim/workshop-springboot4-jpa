20e71eff7cf50132f8491d06c135db0692e27b8e
Leonardo <leo.mota404@gmail.com>
2026-03-10 20:28:21 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,24 +8,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table( name = "tb_product")
-public class Product implements Serializable{
-    
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   
+
     private String nome;
     private String description;
     private Double price;
     private String imgUrl;
 
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(){}
+    public Product() {
+    }
 
     public Product(Long id, String nome, String description, Double price, String imgUrl) {
         this.id = id;
@@ -75,6 +81,10 @@ public class Product implements Serializable{
         this.imgUrl = imgUrl;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -100,5 +110,4 @@ public class Product implements Serializable{
         return true;
     }
 
-    
 }
