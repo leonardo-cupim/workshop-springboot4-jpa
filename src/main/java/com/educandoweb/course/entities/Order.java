cb9b31e7c02bd7b6a4bf26e7497ac569cd7160bf
Leonardo <leo.mota404@gmail.com>
2026-03-11 18:44:30 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -97,6 +97,16 @@ public class Order implements Serializable {
         return this.items;
     }
 
+    public Double getTotal() {
+        Double soma = 0.0;
+        
+        for (OrderItem orderItem : items) {
+            soma += orderItem.getSubTotal();
+        }
+
+        return soma;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -120,11 +130,6 @@ public class Order implements Serializable {
         } else if (!id.equals(other.id))
             return false;
         return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
     }
 
 }
